a2cde81fd0ff993c3cc01b76e092a021918a0f33
Samantha Maher <maher@ecohealthalliance.org>
2020-09-11 12:09:22 -0700

    more scripts

--- R/0.summary-stats.R
+++ R/0.summary-stats.R
@@ -25,3 +25,34 @@ saveRDS(km_stack, P("data_big/mammals_10km_world.RDS"))
 km_stack <- readRDS(P("data_big/mammals_10km_world.RDS"))
 richness_10km <- sum(km_stack)
 writeRaster(richness_10km, P("data_big/richness/richness_world_10km.tif"))
+
+
+#Make Liberia 10 km species map
+library(GADMTools)
+library(rgdal)
+library(pbapply)
+library(rgeos)
+lbr <- getData("GADM", country = "LBR", level = 0) 
+
+
+template <- km_stack[[1]]
+template[template] <- 1
+liberia_mask <- mask(x = template, mask = lbr, updatevalue = 0)
+stack_lbr <- mask(km_stack, lbr, updatevalue = 0, progress = "text")
+
+
+liberia_mask <- function(country, template){
+  mask <- mask(x = template, mask = country, updatevalue = 0)
+  return(mask)
+  #filename <- paste(P("data/predict_masks"), paste(country, ".tif", sep = ""), sep = "/")
+  #writeRaster(mask, filename)
+}
+#liberia_rich <- mask(x = km_stack, mask = lbr, progress = 'text')
+liberia_species <- crop(km_stack, lbr, progress = "text")
+liberia_species <- mask()
+#liberia_species[liberia_species == NA] <- 0
+
+sum_lbr <- sum(liberia_species, progress = "text")
+writeRaster(sum_lbr, P("data_big/richness/liberia_10km.tif"))
+
+            
